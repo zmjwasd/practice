06803015118c5e16122a621befea736c20ef2780
zmj <xiaoming8080@qq.com>
2022-08-24 00:07:39 +0800

    2022.8.23 create branch dev

--- src/practice.java
+++ src/practice.java
@@ -2,5 +2,6 @@ public class practice {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("master to main");
+        System.out.println("create branch dev");
     }
 }
